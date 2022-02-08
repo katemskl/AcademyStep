ab2eff4abf5ed95eb14f354062b7c20d96f22a1b
katemskl <moskal4uk.k@gmail.com>
2022-02-08 18:18:10 +0200

    done 2nd task

--- hm10nasled.py
+++ hm10nasled.py
@@ -15,7 +15,7 @@ class Device:
 
     @staticmethod
     def work():
-        return "I'm working now"
+        return f'Device is working'
 
 
 class CoffeeMachine (Device):
@@ -73,7 +73,81 @@ blender = Blender(power=220, guarantee=1, modes=3)
 meat_grinder = MeatGrinder(power=220, guarantee=2, nozzles=2)
 # print(coffee_machine)
 # print(coffee_machine.make_coffee('espresso'))
-# print(blender)
+# print(blender.work())
 # print(blender.mix())
-print(meat_grinder)
-print(meat_grinder.twist('for sausages'))
+# print(meat_grinder)
+# print(meat_grinder.twist('for sausages'))
+
+
+# task 2
+
+"""Создайте класс Ship, который содержит информацию о корабле.
+С помощью механизма наследования, реализуйте 
+класс Frigate (содержит информацию о фрегате), 
+класс Destroyer (содержит информацию об эсминце), 
+класс Cruiser (содержит информацию о крейсере).
+Каждый из классов должен содержать необходимые для работы методы."""
+
+
+class Ship:
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.speed = kwargs.get('speed')
+        self.fuel = kwargs.get('fuel')
+
+    def sail(self):
+        return f'{self.name} is sailing'
+
+
+class Frigate(Ship):
+
+    def __init__(self, **kwargs):
+        Ship.__init__(self, **kwargs)
+        self.weapons = kwargs.get('weapon')
+
+    def attack(self, count_of_enemies):
+        if self.weapons >= count_of_enemies:
+            return 'The enemy is defeated'
+        return 'You are defeated'
+
+    def __repr__(self):
+        return f'Name of the ship: {self.name}\nMax speed: {self.speed}\n' \
+               f'Fuel: {self.fuel}\nNumber of weapons on board: {self.weapons}'
+
+
+class Destroyer(Ship):
+
+    def __init__(self, **kwargs):
+        Ship.__init__(self, **kwargs)
+        self.displacement = kwargs.get('displacement')
+
+    def destroy_submarines(self, number_of_submarines):
+        if self.displacement >= number_of_submarines:
+            return 'The submarines is destroyed'
+        return 'Not enough displacement'
+
+    def __repr__(self):
+        return f'Name of the ship: {self.name}\nMax speed: {self.speed}\n' \
+               f'Fuel: {self.fuel}\nNumber of displacement: {self.displacement}'
+
+
+class Cruiser(Ship):
+
+    def __init__(self, **kwargs):
+        Ship.__init__(self, **kwargs)
+        self.capacity = kwargs.get('capacity')
+
+    def __repr__(self):
+        return f'Name of the ship: {self.name}\nMax speed: {self.speed}\n' \
+               f'Fuel: {self.fuel}\tCapacity: {self.capacity}'
+
+
+frigate = Frigate(name='Liberty', speed=300, fuel=250, weapon=400)
+print(frigate)
+print(frigate.attack(390))
+destroyer = Destroyer(name='Violet', speed=200, fuel=300, displacement=800)
+print(destroyer)
+print(destroyer.destroy_submarines(60))
+cruiser = Cruiser(name='Georgia', speed=350, fuel=900, capacity=800)
+print(cruiser)
+print(cruiser.sail())
