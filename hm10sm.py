6bd42edb914027abef87da4b215d967d43697247
katemskl <moskal4uk.k@gmail.com>
2022-02-08 11:56:43 +0200

    done 3rd task

--- hm10sm.py
+++ hm10sm.py
@@ -49,6 +49,61 @@ class Degrees:
 
 
 degree = Degrees
-print(degree.celsius_to_fahrenheit(10))
-print(degree.fahrenheit_to_celsius(10))
-print(degree.output_cnt())
+# print(degree.celsius_to_fahrenheit(10))
+# print(degree.fahrenheit_to_celsius(10))
+# print(degree.output_cnt())
+
+
+# task 3
+
+"""Создайте класс для перевода из метрической системы в английскую и наоборот. 
+Функциональность необходимо реализовать в виде статических методов. Обязательно реализуйте перевод мер длины."""
+
+
+class MetricSystem:
+
+    @staticmethod
+    def sm_to_in(sm):
+        return sm / 2.54
+
+    @staticmethod
+    def m_to_ft(m):
+        return m * 3.281
+
+    @staticmethod
+    def m_to_yard(m):
+        return m * 1.094
+
+    @staticmethod
+    def km_to_mile(km):
+        return km/1.609
+
+
+class EnglishSystem:
+    @staticmethod
+    def in_to_sm(inches):
+        return inches * 2.54
+
+    @staticmethod
+    def ft_to_m(ft):
+        return ft / 3.281
+
+    @staticmethod
+    def yard_to_m(yard):
+        return yard / 1.094
+
+    @staticmethod
+    def mile_to_km(mile):
+        return mile * 1.609
+
+
+meter = MetricSystem
+english = EnglishSystem
+# print(meter.sm_to_in(20))
+# print(meter.m_to_ft(1))
+# print(meter.m_to_yard(1))
+# print(meter.km_to_mile(10))
+# print(english.in_to_sm(34))
+# print(english.ft_to_m(10))
+# print(english.yard_to_m(20))
+# print(english.mile_to_km(20))
