ff37a091115700fb1b7b12d385366a8ff1a2a1c3
katemskl <moskal4uk.k@gmail.com>
2022-06-16 19:08:37 +0300

    added code for game

---
+++ main5.py
@@ -0,0 +1,49 @@
+import random
+
+print('Welcome to game')
+print('[r] - rock\n[s] - scissors\n[p] - paper')
+
+user_score = 0
+bot_score = 0
+variants = ['r', 's', 'p']
+
+print('---- BEGINNING ----')
+for i in range(3):
+    print(f'---- ROUND #{i+1} ----')
+    user_choice = input('Your choice: ')
+    bot_choice = random.choice(variants)
+    print(f'\tYou: {user_choice}\tbot: {bot_choice}')
+    if user_choice == bot_choice:
+        print('Draw')
+        user_score += 1
+        bot_score += 1
+    elif user_choice == 'r':
+        if bot_choice == 's':
+            user_score += 1
+            print('Player won!')
+        else:
+            bot_score += 1
+            print('Bot won!')
+    elif user_choice == 's':
+        if bot_choice == 'p':
+            user_score += 1
+            print('Player won!')
+        else:
+            bot_score += 1
+            print('Bot won!')
+    elif user_choice == 'p':
+        if bot_choice == 'r':
+            user_score += 1
+            print('Player won!')
+        else:
+            bot_score += 1
+            print('Bot won!')
+    else:
+        print('Something wrong')
+    print('-'*20)
+if user_score > bot_score:
+    print(f'Player won game with score {user_score}')
+elif user_score < bot_score:
+    print(f'Bot won game with score {bot_score}')
+else:
+    print(f'Draw! {user_score}:{bot_score}')
